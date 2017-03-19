34e308d0536e517970ca6f786743867ac39e1080
Alexandru Ghirua <ghalex@gmail.com>
2017-03-19 19:44:19 +0200

    Add initial files

---
+++ application/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.start = function (container) {
+	console.log("app started...");
+}
+
+/** Start app */
+document.startApp(document.getElementById("app"));
